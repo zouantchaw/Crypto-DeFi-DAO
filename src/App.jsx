deb17afb4e7787b772c50185ee233b87da1e242f
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2022-01-20 19:30:11 +0000

    add connect to wallet button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // Use the connectWallet hook thirdweb gives us.
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  // If user hasn't connected their wallet
+  // to web app. Call connectWallet
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Crypto/DeFi DAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  
+  // If users wallet is connected
+  // Show welcome message
   return (
     <div className="landing">
-      <h1>Crypto∕Defi DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
 
 export default App;
